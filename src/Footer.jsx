cca2756da4e548e621580e26e096e0bb9cf1f90e
Boston Janisheck <bostonjanisheck@gmail.com>
2024-08-06 17:44:27 -0500

    component files created, styling added

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyright by @/boston-janisheck</small>
+      <small>Last checked limits: 12 days ago</small>
+    </footer>
+  );
+};
+
+export default Footer;
