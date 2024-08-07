bda06ca9105520d3d90ef53387beb6a50e00135f
Boston Janisheck <bostonjanisheck@gmail.com>
2024-08-07 13:04:30 -0500

    controlled textarea created

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const Textarea = () => {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  return (
+    <textarea
+      value={text}
+      onChange={(e) => {
+        setText(e.target.value);
+        console.log(e.target.value);
+      }}
+      className="textarea"
+      placeholder="Start typing..."
+      spellCheck="false"
+    />
+  );
 };
 
 export default Textarea;
