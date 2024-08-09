6599ed951e526c3cd9bf73141303d085bf382ca3
Boston Janisheck <bostonjanisheck@gmail.com>
2024-08-08 21:58:18 -0500

    no script tags in textarea rule created

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 const Textarea = () => {
   const [text, setText] = useState("");
+  const [warning, setWarning] = useState("");
+  const handleChange = (e) => {
+    let text = e.target.value;
+    if (text.includes("<script>")) {
+      setWarning("Scripts are not allowed.");
+      text = text.replace("<script>", "");
+    } else {
+      setWarning("");
+    }
+    setText(text);
+    console.log(text);
+  };
   return (
-    <textarea
-      value={text}
-      onChange={(e) => {
-        setText(e.target.value);
-        console.log(e.target.value);
-      }}
-      className="textarea"
-      placeholder="Start typing..."
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Start typing..."
+        spellCheck="false"
+      />
+      <Warning warningText={warning} />
+    </div>
   );
 };
 
